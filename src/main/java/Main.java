ed5c1ca436e02a94d18a6c2d647fd90f5e206cad
jbrennan7651 <103645942+jbrennan7651@users.noreply.github.com>
2022-04-25 21:46:40 -0400

    Add readme

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My name is James Brennan.");
         System.out.println("this is a process");
+        System.out.println("majestic");
     }
 }
